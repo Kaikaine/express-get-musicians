c373ee8b14ef277102fe321de46e57b61f84847a
kaikaine <kairijb@protonmail.com>
2023-11-20 15:50:33 -0600

    added tests for route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-app.get('/musicians', async(req, res) =>{
-    const data = await Musician.findAll()
-    console.log(data)
-    res.json(data)
-})
+//TODO: Create a GET /musicians route to return all musicians
+app.get("/musicians", async (req, res) => {
+  const data = await Musician.findAll();
+  console.log(data);
+  res.json(data);
+});
 
-
-
-
-
-module.exports = { app };
+module.exports = app;
