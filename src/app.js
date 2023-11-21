1f620e83217e790d604b31ad514d1219164e552a
Chase AK <109045340+achasek@users.noreply.github.com>
2023-11-21 14:07:07 -0700

    added post (#4)

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ const { Musician } = require("../models/index");
 const { db } = require("../db/connection");
 
 const port = 3000;
+app.use(express.json());
 
 //TODO: Create a GET /musicians route to return all musicians
 app.get("/musicians", async (req, res) => {
   const data = await Musician.findAll();
-  console.log(data);
+  // console.log(data);
   res.json(data);
 });
 
@@ -17,5 +18,10 @@ app.get("/musicians/:id", async (req, res) => {
   const data = await Musician.findByPk(id);
   res.json(data);
 });
+
+app.post('/musicians', async (req, res) => {
+  const newMusician = await Musician.create(req.body);
+  res.status(200).json(newMusician);
+})
 
 module.exports = app;
